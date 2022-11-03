279184564809fcffa56d325002c8b0a66eeafcdc
suqun zhang <susizhang2017@gmail.com>
2022-11-03 18:23:43 +0100

    Adding some files

--- cookbook-advert/src/Group4/Group4.js
+++ cookbook-advert/src/Group4/Group4.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { client } from "../client";
+
+// console.log(client);
 
 const Group4 = () => {
   const [isGrop4Loading, setGroup4Loading] = useState(false);
   const [group4Slides, setGroup4Slides] = useState([]);
 
-  const getGroup4Slides = async () => {
+  const getGroup4Slides = useCallback(async () => {
     try {
       const response = await client.getEntries({
         content_type: "kitchenGroup4",
       });
-      console.log(response);
+      // console.log(response);
+      const responseData = response.items;
+      console.log(responseData);
     } catch (error) {
       console.log(error);
     }
-  };
+  }, []);
 
   useEffect(() => {
     getGroup4Slides();
-  }, []);
+  }, [getGroup4Slides]);
 
   return (
     <div>
