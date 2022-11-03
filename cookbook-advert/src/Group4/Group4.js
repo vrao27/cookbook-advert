1e08c2853f5472fd262a5835f8f017cffb27b5fa
suqun zhang <susizhang2017@gmail.com>
2022-11-03 20:41:21 +0100

    Adding style files

--- cookbook-advert/src/Group4/Group4.js
+++ cookbook-advert/src/Group4/Group4.js
@@ -1,32 +1,67 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { client } from "../client";
+import Group4Slide from "./Group4Slide";
+import "./Group4.css";
 
 // console.log(client);
 
 const Group4 = () => {
-  const [isGrop4Loading, setGroup4Loading] = useState(false);
+  const [isGrop4Loading, setIsGroup4Loading] = useState(false);
   const [group4Slides, setGroup4Slides] = useState([]);
 
+  // clean up data
+
+  const cleanUpGroup4Slides = useCallback((rawData) => {
+    const cleanSlides = rawData.map((slide) => {
+      const { sys, fields } = slide;
+      const { id } = sys;
+      const slideTitle = fields.title;
+      const slideDescription = fields.description;
+      const slideBg = fields.image.fields.file.url;
+      const updatedSlide = { id, slideTitle, slideDescription, slideBg };
+      return updatedSlide;
+    });
+    setGroup4Slides(cleanSlides);
+  }, []);
+
   const getGroup4Slides = useCallback(async () => {
+    setIsGroup4Loading(true);
     try {
       const response = await client.getEntries({
         content_type: "kitchenGroup4",
       });
       // console.log(response);
       const responseData = response.items;
-      console.log(responseData);
+      // console.log(responseData);
+      responseData ? cleanUpGroup4Slides(responseData) : setGroup4Slides([]);
+      setIsGroup4Loading(false);
     } catch (error) {
       console.log(error);
+      setIsGroup4Loading(false);
     }
-  }, []);
+  }, [cleanUpGroup4Slides]);
 
   useEffect(() => {
     getGroup4Slides();
   }, [getGroup4Slides]);
 
+  // console.log(group4Slides);
+  if (isGrop4Loading) {
+    return <h2>Loading...</h2>;
+  }
   return (
     <div>
-      <h1>Kitchen Group4</h1>
+      {group4Slides.map((item) => {
+        const { id, slideBg, slideTitle, slideDescription } = item;
+        return (
+          <Group4Slide
+            slideTitle={slideTitle}
+            slideBg={slideBg}
+            slideDescription={slideDescription}
+            key={id}
+          />
+        );
+      })}
     </div>
   );
 };
